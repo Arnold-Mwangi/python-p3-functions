cbd0d4f5959b4536f331205bb156c5d23bfe860f
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-08-17 16:47:42 +0300

    Completed Task

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+# def greet_programmer():
+#     print("Hello, programmer!")
 
-def greet(name):
-    pass
 
-def greet_with_default(name="programmer"):
-    pass
+# def greet(name):
+#     print(f"Hello, {name}!")
+
+# def greet_with_default(name="programmer"):
+#     print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+
+
+
+
+
